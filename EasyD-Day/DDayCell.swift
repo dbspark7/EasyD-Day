acee64954f5642470819e70375fbb92302aea849
Soo Seong Park <dbspark7@icloud.com>
2017-10-26 11:30:39 +0900

    1단계

---
+++ EasyD-Day/DDayCell.swift
@@ -0,0 +1,16 @@
+//
+//  DDayCell.swift
+//  EasyD-Day
+//
+//  Created by 박수성 on 2017. 10. 24..
+//  Copyright © 2017년 dbspark7. All rights reserved.
+//
+
+import UIKit
+
+class DDayCell: UITableViewCell {
+    
+    @IBOutlet weak var dday: UILabel!
+    @IBOutlet weak var content: UILabel!
+    @IBOutlet weak var notification: UILabel!
+}
