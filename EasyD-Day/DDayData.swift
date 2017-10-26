f7853c843bab9c75eaa803edfd0106aaaa59dd2a
Soo Seong Park <dbspark7@icloud.com>
2017-10-26 11:30:47 +0900

    1단계

---
+++ EasyD-Day/DDayData.swift
@@ -0,0 +1,18 @@
+//
+//  DDayData.swift
+//  EasyD-Day
+//
+//  Created by 박수성 on 2017. 10. 24..
+//  Copyright © 2017년 dbspark7. All rights reserved.
+//
+
+import Foundation
+
+class DDayData {
+    
+    var date: Date? // 디데이 날짜
+    var message: String? // 메세지 내용
+    var timeAlert: Date? // 알림 시각
+    var alertIsOn: Bool? // 알림 사용 여부
+    
+}
