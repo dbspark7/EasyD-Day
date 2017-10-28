a248d037e7889188870af7c40cf5de90f7019721
Soo Seong Park <dbspark7@icloud.com>
2017-10-28 10:56:16 +0900

    두 번째

--- EasyD-Day/DDayViewController.swift
+++ EasyD-Day/DDayViewController.swift
@@ -7,11 +7,34 @@
 //
 
 import UIKit
+import UserNotifications
 
 class DDayViewController: UITableViewController {
     
     // 앱 델리게이트 객체의 참조 정보를 읽어온다.
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    // D-Day 알림 메소드
+    func ddayAlert(message: String, setTime: Date, identifier: String) -> Void {
+        
+        // 알림 컨텐츠 정의
+        let nContent = UNMutableNotificationContent()
+        nContent.body = message
+        nContent.title = "D-Day"
+        nContent.sound = UNNotificationSound.default()
+        
+        // 발송 시각을 '지금으로부터 *초 형식'으로 변환
+        let time = setTime.timeIntervalSinceNow
+        
+        // 발송 조건 정의
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: time, repeats: false)
+        
+        // 발송 요청 객체 정의
+        let request = UNNotificationRequest(identifier: identifier, content: nContent, trigger: trigger)
+        
+        // 노티피케이션 센터에 추가
+        UNUserNotificationCenter.current().add(request)
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let count = self.appDelegate.ddaylist.count
@@ -37,10 +60,12 @@ class DDayViewController: UITableViewController {
         
         cell.content?.text = row.message
         
-        if row.alertIsOn! {
+        if row.alertIsOn! && (row.timeAlert?.timeIntervalSinceNow)! > 0 {
             let formatter = DateFormatter()
             formatter.dateFormat = "yyyy.MM.dd ahh:mm에 알림"
             cell.notification?.text = formatter.string(from: row.timeAlert!)
+            
+            self.ddayAlert(message: row.message!, setTime: row.timeAlert!, identifier: String(indexPath.row))
         } else {
             cell.notification?.text = "알림 없음"
         }
@@ -71,6 +96,7 @@ class DDayViewController: UITableViewController {
         if self.appDelegate.ddaylist.count == 0 {
             self.navigationItem.leftBarButtonItem = nil
         }
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [String(indexPath.row)])
     }
     
     @IBAction func backToList(_ segue: UIStoryboardSegue) {
